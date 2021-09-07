f1dd53db5a17bda698943fd75a40c091567d0b56
Eik <etty.ikpeme@gmail.com>
2021-09-07 14:16:08 -0500

    updated portfolio

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,14 +1,16 @@
 import css1 from "../img/portImages/cv_css.png";
 import css2 from "../img/portImages/ms.png";
 import react1 from "../img/portImages/react_styled.png";
-import img2 from "../img/portImages/maya-4.jpg";
+import img1 from "../img/portImages/maya-1.jpg";
+import img2 from "../img/portImages/maya-2.jpg";
 import img3 from "../img/portImages/maya-3.jpg";
+import img4 from "../img/portImages/maya-4.jpg";
 
 const portfolios = [
   {
     id: 1,
     category: "React",
-    image: img3,
+    image: img1,
     link1: "https://github.com/eikpeme/GraceShopper",
     link2: "https://grayce-shoppa.herokuapp.com/",
     title: "Grayce-Shoppa",
@@ -17,16 +19,16 @@ const portfolios = [
   {
     id: 2,
     category: "React",
-    image: img3,
+    image: img2,
     link1: "https://github.com/eikpeme/2104-FSA-RM-WEB-FT-JPFP",
     link2: "https://ninja-academy.herokuapp.com/campuses",
     title: "Academy App",
     text: "Student Campus directory demonstrating CRUD functionality",
   },
   {
-    id: 4,
+    id: 3,
     category: "React Native",
-    image: img2,
+    image: img3,
     link1: "https://github.com/eikpeme/parkapp/tree/main",
     link2: "https://youtu.be/PPomOi4U0kk",
     title: "Park App",
@@ -34,9 +36,9 @@ const portfolios = [
       "Parking meets Air bnb. Park app is a community based mobile parking applicaiton where users can provide parking and rent parking to other user ",
   },
   {
-    id: 5,
+    id: 4,
     category: "React Native",
-    image: img2,
+    image: img4,
     link1: "https://www.google.com",
     link2: "https://www.google.com",
     title: "Todoz App",
