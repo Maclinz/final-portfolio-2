1ba07b0eaa0fbb6020ab0032a728e396c9c90add
Eik <etty.ikpeme@gmail.com>
2021-09-15 08:52:40 -0500

    Adjusted footer

--- src/Components/ServicesSection.js
+++ src/Components/ServicesSection.js
@@ -28,7 +28,6 @@ function ServicesSection() {
                 "Work involved in developing a Web site for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services."
               }
             />
-            d
           </div>
           <ServiceCard
             image={gamedev}
